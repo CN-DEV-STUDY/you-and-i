8f0aefa596b7e468c321ce134faed66f37b24cc5
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-09-10 12:14:39 +0900

    feat: add period css

--- src/components/PeriodCard.tsx
+++ src/components/PeriodCard.tsx
@@ -5,7 +5,12 @@ import periodImg from "./../assets/period.jpg";
 function PeriodCard() {
   return (
     <Container>
-      <RoundCard />
+      <RoundCard>
+        <Text fontWeight={"bold"} fontSize={24}>
+          450일
+        </Text>
+        <Text justifyContent={"flex-end"}>2022.06.18(토)</Text>
+      </RoundCard>
     </Container>
   );
 }
@@ -17,14 +22,33 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   padding: 0 10px;
+  margin-bottom: 20px;
 `;
 const RoundCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
   width: 100%;
   margin-top: 10px;
-  height: 130px;
+  padding: 10px 20px;
+  height: 120px;
   border-radius: 10px;
   background-color: var(--color__white);
   background-image: url(${periodImg});
   background-size: cover;
   background-position: center bottom;
 `;
+
+const Text = styled.div<TextStyles>`
+  display: flex;
+  color: var(--color__white);
+  font-weight: ${(props) => props.fontWeight};
+  font-size: ${(props) => props.fontSize}px;
+  justify-content: ${(props) => props.justifyContent};
+`;
+
+type TextStyles = {
+  fontWeight?: string;
+  fontSize?: number;
+  justifyContent?: string;
+};
