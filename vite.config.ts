4d00f24611250ca7ee2af90c6adf9c1946bb0be5
nohyunha95 <nohyunha95@gmail.com>
2023-10-17 08:44:36 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import {defineConfig, loadEnv} from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
+import {resolve} from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        },
+      },
+    },
+    define: {
+      // VITE_MY_VARIABLE 환경 변수를 로드합니다.
+      'import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY': process.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY,
+      'import.meta.env.VITE_BASE_URL': process.env.VITE_BASE_URL,
+    },
     plugins: [react()],
     server: {
       host: true,
@@ -17,4 +30,6 @@ export default defineConfig(({command, mode}) => {
       },
     },
   }
+
+
 })
