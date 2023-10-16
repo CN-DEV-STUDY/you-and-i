95aaace82b0fec7ce2b99dabf4d8419eb4426f0f
nohyunha95 <nohyunha95@gmail.com>
2023-10-17 02:28:19 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
+    port: 5173
   },
   resolve: {
     alias: {
