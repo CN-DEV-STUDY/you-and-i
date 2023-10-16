44429919519f51e72434e0729ea2663f4538daa5
nohyunha95 <nohyunha95@gmail.com>
2023-10-16 23:11:03 +0900

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml up -d --build
+# Set the path to the docker-compose executable
+DOCKER_COMPOSE_PATH=/path/to/docker-compose
+
+# Check if docker-compose is available
+if command -v "$DOCKER_COMPOSE_PATH" &> /dev/null
+then
+    # docker-compose is available, so run your commands
+    $DOCKER_COMPOSE_PATH up -d
+else
+    # docker-compose is not available
+    echo "Error: docker-compose not found. Please make sure it's installed and in your PATH."
+    exit 1
+fi
