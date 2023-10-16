a0d10cdae05bc9822b1d825208ae16daa2055674
nohyunha95 <nohyunha95@gmail.com>
2023-10-17 07:54:12 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5173
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
 })
