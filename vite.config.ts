0e70f78eb2f7fc63d87fb965afe85f75904ad8c9
nohyunha95 <nohyunha95@gmail.com>
2023-10-17 08:40:56 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({command, mode}) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
     plugins: [react()],
     server: {
       host: true,
