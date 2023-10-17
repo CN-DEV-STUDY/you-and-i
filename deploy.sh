8dc11c13f4974376d1ab5446ab2ef7f3e8bcb770
nohyunha95 <nohyunha95@gmail.com>
2023-10-17 14:50:25 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#docker stop $(docker ps -a -q)
-#sleep 3
-#docker rm $(docker ps -aq)
-#sleep 3
-#docker rmi nohyunha95/you-and-i-frontend
-#sleep 3
+docker stop $(docker ps -a -q)
+sleep 3
+docker rm $(docker ps -aq)
+sleep 3
+docker rmi nohyunha95/you-and-i-frontend
+sleep 3
 
-docker-compose -f ../docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d
 ## Set the path to the docker-compose executable
 #DOCKER_COMPOSE_PATH=$(command -v docker-compose)
 #
