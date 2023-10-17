7ed7b661360184523ab127eafa1246653f8f172c
nohyunha95 <nohyunha95@gmail.com>
2023-10-17 09:13:40 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,19 @@
-import {defineConfig, loadEnv} from 'vite'
+import {defineConfig} from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
-import {resolve} from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
-  return {
-
-    plugins: [react(),
-      require("@import-meta-env/unplugin").webpack({
-        env: ".env",
-        example: ".env.example",
-        transformMode: "compile-time",
-        include: ["VITE_*", "NODE_ENV"],
-      }),
-    ],
-    server: {
-      host: true,
-      port: 5173
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  }
+  },
 
 
 })
