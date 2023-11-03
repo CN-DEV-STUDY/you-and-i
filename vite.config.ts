a603f98df5bd47be4b5ae94120d11c544ddeb8f2
nohyunha95 <nohyunha95@gmail.com>
2023-11-03 20:37:28 +0900

    로그인 안되는 버그 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/login",
-
+  plugins: [react()],
   // https://stackoverflow.com/questions/75883720/504-outdated-optimize-dep-while-using-react-vite
   // optimizeDeps: {
   //   exclude: ['js-big-decimal']
   // },
+
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 
   resolve: {
     alias: {
